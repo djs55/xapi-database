a497dddac72cd94d3ecd28f54b241706568eedc0
David Scott <dave.scott@eu.citrix.com>
2014-11-06 21:53:39 +0000

    Write the table / row / key / value to Irmin
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- core/db_git.ml
+++ core/db_git.ml
@@ -10,5 +10,17 @@ module To = struct
     let module Store = Git.Make(IrminKey.SHA1)(IrminContents.String)(IrminTag.String) in
     Store.create () >>=
       (fun store ->
-        Store.update store ["foo"; "bar"] "stuff")
+        let open Db_cache_types in
+        let table name _ _ (tbl: Table.t) acc =
+          let record rf ctime mtime (row: Row.t) acc =
+            let preamble =
+              [("__mtime",Int64.to_string mtime); ("__ctime",Int64.to_string ctime); ("ref",rf)] in
+            let pairs = Row.fold (fun k _ _ v acc -> (k, Xml_spaces.protect v) :: acc) row preamble in
+            Lwt_list.iter_s
+              (fun (k, v) ->
+                Store.update store [ "xapi"; name; rf; k ] v
+              ) pairs :: acc in
+          Lwt.join (Table.fold record tbl []) :: acc in
+        Lwt.join (TableSet.fold table (Database.tableset db) [])
+      )
 end
