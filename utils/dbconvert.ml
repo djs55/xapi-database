b53fae7adc3d9a2ec8ed8dad22805c9084ad5eb2
David Scott <dave.scott@eu.citrix.com>
2014-11-06 21:20:48 +0000

    Load the database schema exported from xapi
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- utils/dbconvert.ml
+++ utils/dbconvert.ml
@@ -6,12 +6,16 @@ let usage () =
   Printf.printf "./%s new-xml <db-path>\n%!" name;
   Printf.printf "./%s convert <db-path> <git-path>\n%!" name
 
+let load_schema () =
+  let sexp = Sexplib.Sexp.load_sexp "db.schema" in
+  Schema.t_of_sexp sexp
+
 let () =
   match Sys.argv with
   | [|_; "new-xml"; db_path|] ->
-    let db = Database.make Test_schemas.schema in
+    let db = Database.make (load_schema ()) in
     Db_xml.To.file db_path db
   | [|_; "convert"; db_path; git_path|] ->
-    let db = Db_xml.From.file Test_schemas.schema db_path in
+    let db = Db_xml.From.file (load_schema ()) db_path in
     Lwt_main.run (Db_git.To.file git_path db)
   | _ -> usage ()
