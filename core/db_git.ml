aa13c2c8ef9a684f08b6269741bf9aff138ff8e6
David Scott <dave.scott@eu.citrix.com>
2014-11-07 09:20:40 +0000

    Serialise conversion (avoiding running out of fds etc)
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- core/db_git.ml
+++ core/db_git.ml
@@ -5,7 +5,7 @@ module To = struct
   let file (path: string) db : unit Lwt.t =
     let module Git = IrminGit.FS(struct
       let root = Some path
-      let bare = false
+      let bare = true
     end) in
     let module Store = Git.Make(IrminKey.SHA1)(IrminContents.String)(IrminTag.String) in
     Store.create () >>=
@@ -16,11 +16,9 @@ module To = struct
             let preamble =
               [("__mtime",Int64.to_string mtime); ("__ctime",Int64.to_string ctime); ("ref",rf)] in
             let pairs = Row.fold (fun k _ _ v acc -> (k, Xml_spaces.protect v) :: acc) row preamble in
-            Lwt_list.iter_s
-              (fun (k, v) ->
-                Store.update store [ "xapi"; name; rf; k ] v
-              ) pairs :: acc in
-          Lwt.join (Table.fold record tbl []) :: acc in
-        Lwt.join (TableSet.fold table (Database.tableset db) [])
+            List.map (fun (k, v) -> [ "xapi"; name; rf; k ], v) pairs @ acc in
+          Table.fold record tbl [] @ acc in
+        let pairs = TableSet.fold table (Database.tableset db) [] in
+        Lwt_list.iter_s (fun (k, v) -> Store.update store k v) pairs
       )
 end
