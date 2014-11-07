f2dd282168f3f45dd488f3e09da0d8618bd894dc
David Scott <dave.scott@eu.citrix.com>
2014-11-07 09:25:37 +0000

    Add the database in one large transaction
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- core/db_git.ml
+++ core/db_git.ml
@@ -5,12 +5,14 @@ module To = struct
   let file (path: string) db : unit Lwt.t =
     let module Git = IrminGit.FS(struct
       let root = Some path
-      let bare = true
+      let bare = false
     end) in
     let module Store = Git.Make(IrminKey.SHA1)(IrminContents.String)(IrminTag.String) in
     Store.create () >>=
       (fun store ->
         let open Db_cache_types in
+        Store.View.of_path store []
+        >>= fun view ->
         let table name _ _ (tbl: Table.t) acc =
           let record rf ctime mtime (row: Row.t) acc =
             let preamble =
@@ -19,6 +21,12 @@ module To = struct
             List.map (fun (k, v) -> [ "xapi"; name; rf; k ], v) pairs @ acc in
           Table.fold record tbl [] @ acc in
         let pairs = TableSet.fold table (Database.tableset db) [] in
-        Lwt_list.iter_s (fun (k, v) -> Store.update store k v) pairs
+        Lwt_list.iter_s (fun (k, v) -> Store.View.update view k v) pairs
+        >>= fun () ->
+        let origin = IrminOrigin.create "Initial import of %s" path in
+        Store.View.merge_path ~origin store [] view
+        >>= function
+        | `Ok () -> return ()
+        | `Conflict msg -> failwith (Printf.sprintf "Conflict during final merge: %s" msg)
       )
 end
